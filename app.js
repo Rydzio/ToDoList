d8cbeb09d8d8b2f9b458a9737d58caddde42989a
rytych <michal.rytych@geminibs.com>
2022-07-29 11:54:45 +0200

    Week days improve

--- app.js
+++ app.js
@@ -11,43 +11,45 @@ app.get("/", function(req, res) {
   var today = new Date();
   var currentDay = today.getDay();
   var day = "";
+  const weekDays = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
 
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-
-    case 1:
-      day = "Monday";
-      break;
-
-    case 2:
-      day = "Tuesday";
-      break;
-
-    case 3:
-      day = "Wednesday";
-      break;
-
-    case 4:
-      day = "Thursday";
-      break;
-
-    case 5:
-      day = "Friday";
-      break;
-
-    case 6:
-      day = "Saturday";
-      break;
+  for (var i = 0; i < weekDays.length; i++) {
+    if (currentDay === i) {
+      day = weekDays[i];
+    }
   }
 
-  // if (currentDay === 6 || 0) {
-  //   day = "Weekend";
+  // switch (currentDay) {
+  //   case 0:
+  //     day = "Sunday";
+  //     break;
   //
-  // } else {
-  //   day = "Weekday";
+  //   case 1:
+  //     day = "Monday";
+  //     break;
+  //
+  //   case 2:
+  //     day = "Tuesday";
+  //     break;
+  //
+  //   case 3:
+  //     day = "Wednesday";
+  //     break;
+  //
+  //   case 4:
+  //     day = "Thursday";
+  //     break;
+  //
+  //   case 5:
+  //     day = "Friday";
+  //     break;
+  //
+  //   case 6:
+  //     day = "Saturday";
+  //     break;
+  //
+  //   default:
+  //     console.log("Error: current day is equal to: " + currentDay);
   // }
 
   res.render("list", {
