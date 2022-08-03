eaf59c760ef0ad729efca36ccb8638f27df642a9
rytych <michal.rytych@geminibs.com>
2022-08-03 12:58:49 +0200

    Complete Add Position

--- app.js
+++ app.js
@@ -5,59 +5,54 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-  const weekDays = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
 
-  for (var i = 0; i < weekDays.length; i++) {
-    if (currentDay === i) {
-      day = weekDays[i];
-    }
-  }
+  // var day = getSpanishDay(today.getDay());
+  var day = getDay(today);
 
-  // switch (currentDay) {
-  //   case 0:
-  //     day = "Sunday";
-  //     break;
-  //
-  //   case 1:
-  //     day = "Monday";
-  //     break;
-  //
-  //   case 2:
-  //     day = "Tuesday";
-  //     break;
-  //
-  //   case 3:
-  //     day = "Wednesday";
-  //     break;
-  //
-  //   case 4:
-  //     day = "Thursday";
-  //     break;
-  //
-  //   case 5:
-  //     day = "Friday";
-  //     break;
-  //
-  //   case 6:
-  //     day = "Saturday";
-  //     break;
-  //
-  //   default:
-  //     console.log("Error: current day is equal to: " + currentDay);
-  // }
-
-  res.render("list", {
-    kindOfDay: day
+  res.render("list", { //gets sent to list.ejs
+    kindOfDay: day,
+    newListItems: items
   });
 
 });
 
-app.listen(3000, function() {
+app.post("/", (req, res) => {
+  var item = req.body.newItem; //to use req.body we need to set up bodyParser - app.use(bodyParser.urlencoded({ extended: true }));
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
+
+// Day Functions 
+
+function getDay(today) {
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  }
+  var day = today.toLocaleDateString("en-US", options)
+  return (day);
+}
+
+function getSpanishDay(currentDay) {
+  const weekDays = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+  for (var i = 0; i < weekDays.length; i++) {
+    if (currentDay === i) {
+      var day = weekDays[i];
+    }
+  }
+  return day;
+}
